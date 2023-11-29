f5b254ef358f0febdafff07f5cae61abd4bd3762
slipsky38 <samantha.lipsky@takeda.com>
2023-11-29 19:37:29 +0000

    added ends_with parameter

--- api/main.py
+++ api/main.py
@@ -4,12 +4,12 @@ import fastapi
 app = fastapi.FastAPI()
 
 @app.get("/generate_name")
-async def generate_name(starts_with:str = None):
+async def generate_name(starts_with:str = 'n', ends_with: str='a'):
     names = ["Minnie", "Margaret", "Myrtle", "Noa", "Nadia"]
     if starts_with is not None:
-        filtered_names = [name for name in names if name.lower().startswith(starts_with.lower())]
+        filtered_names = [name for name in names if name.lower().startswith(starts_with.lower()) and name.lower().endswith(ends_with.lower)]
         if len(filtered_names) == 0:
-            raise fastapi.HTTPException(status_code=404, detail="No names found with that start letter.")
+            raise fastapi.HTTPException(status_code=404, detail="No name found.")
         random_name = random.choice(filtered_names)
     else:
         random_name = random.choice(names)
