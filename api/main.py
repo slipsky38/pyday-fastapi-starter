e12646af94e61ba317a4151f7eea272406a14e45
slipsky38 <samantha.lipsky@takeda.com>
2023-11-29 19:13:58 +0000

    worked up to second gdi break of training

--- api/main.py
+++ api/main.py
@@ -1,11 +1,16 @@
 import random
-
 import fastapi
 
 app = fastapi.FastAPI()
 
 @app.get("/generate_name")
-async def generate_name():
+async def generate_name(starts_with:str = None):
     names = ["Minnie", "Margaret", "Myrtle", "Noa", "Nadia"]
-    random_name = random.choice(names)
+    if starts_with is not None:
+        filtered_names = [name for name in names if name.lower().startswith(starts_with.lower())]
+        if len(filtered_names) == 0:
+            raise fastapi.HTTPException(status_code=404, detail="No names found with that start letter.")
+        random_name = random.choice(filtered_names)
+    else:
+        random_name = random.choice(names)
     return {"name": random_name}
